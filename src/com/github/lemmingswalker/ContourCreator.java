5228bf2b0c3f08845747ed4113772a7812670e81
Doeke Wartena <clankill3r@gmail.com>
2014-12-12 03:29:57 +0100

    -ContourWalker calculates x and y + min and max values now -Updated Interface to x and y and simplified some methods

--- src/com/github/lemmingswalker/ContourCreator.java
+++ src/com/github/lemmingswalker/ContourCreator.java
@@ -9,12 +9,16 @@ public interface ContourCreator {
 
     public boolean checkForExistingBlob(int index, int x, int y);
 
-    public void startContour(int startIndex, int[] pixels, int w, int h);
-    public void contourCreationFail();
-    public void finishContour(int[] pixels, int w, int h);
+    public void startContour(int startIndex, int[] pixels, int imageWidth, int imageHeight);
+    public void contourCreationFail(); // String reason?
+    public void finishContour(int[] pixels, int imageWidth, int imageHeight);
 
-    public void addToCornerIndexes(int index);
-    public void addToEdgeIndexes(int index);
+    public void addCorner(int index, int x, int y);
+    public void addEdge(int index, int x, int y);
+
+    public void setMinAndMaxCornerValues(int minXIndex, int minX, int minYIndex, int minY, int maxXIndex, int maxX, int maxYIndex, int maxY);
+
+    public void isOuterContour(boolean isOuterContour);
 
     public void finishOfScan();
 
--- src/com/github/lemmingswalker/ContourWalker.java
+++ src/com/github/lemmingswalker/ContourWalker.java
@@ -75,17 +75,26 @@ public class ContourWalker {
 
 
         int current = startIndex;
+        int x = current % imageWidth;
+        int y = (current - x) / imageWidth;
 
-        valueDown = thresholdChecker.check(pixels[current+DOWN]);
-        valueRight = thresholdChecker.check(pixels[current+RIGHT]);
-        valueUp = thresholdChecker.check(pixels[current+UP]);
-        valueLeft = thresholdChecker.check(pixels[current+LEFT]);
+
+        int minX, minY, maxX, maxY;
+        minX = minY = Integer.MAX_VALUE;
+        maxX = maxY = Integer.MIN_VALUE;
+        int minXCornerIndex, maxXCornerIndex, minYCornerIndex, maxYCornerIndex;
+        minXCornerIndex = maxXCornerIndex = minYCornerIndex = maxYCornerIndex = -1;
+
+        valueDown = thresholdChecker.check(pixels[current + DOWN]);
+        valueRight = thresholdChecker.check(pixels[current + RIGHT]);
+        valueUp = thresholdChecker.check(pixels[current + UP]);
+        valueLeft = thresholdChecker.check(pixels[current + LEFT]);
 
 
         // or we don't add it!
         downIsFree = valueDown >= threshold;
         rightIsFree = valueRight >= threshold;
-        upIsFree =  valueUp >= threshold;
+        upIsFree = valueUp >= threshold;
         leftIsFree = valueLeft >= threshold;
 
         // todo: 1 line starting point blob
@@ -94,16 +103,16 @@ public class ContourWalker {
         // figure out if the start is like a single line
         // if that is true then we have to scan in the other direction as well
         // maybe set a boolean that the first cornerPixel is not a cornerPixel
-        float valueDownLeft = thresholdChecker.check(pixels[current+DOWN+LEFT]);
-        float valueDownRight = thresholdChecker.check(pixels[current+DOWN+RIGHT]);
-        float valueUpLeft = thresholdChecker.check(pixels[current+UP+LEFT]);
-        float valueUpRight = thresholdChecker.check(pixels[current+UP+RIGHT]);
+        float valueDownLeft = thresholdChecker.check(pixels[current + DOWN + LEFT]);
+        float valueDownRight = thresholdChecker.check(pixels[current + DOWN + RIGHT]);
+        float valueUpLeft = thresholdChecker.check(pixels[current + UP + LEFT]);
+        float valueUpRight = thresholdChecker.check(pixels[current + UP + RIGHT]);
 
         // todo: onLine check, this should work, 1st = horizontal line 2nd = vertical line
         // onLine = upIsFree && downIsFree ? true : leftIsFree && rightIsFree ? true : false;
 
         // false on isolated pixel
-        if(!setStartDirection()) {
+        if (!setStartDirection()) {
             // we can't get a blob out of this
             // we can't reject the blob since rejected blobs
             // are valid!
@@ -113,61 +122,91 @@ public class ContourWalker {
         }
 
 
-
         int c = 0;
         if (!leftIsFree) c++;
         if (!rightIsFree) c++;
         if (!upIsFree) c++;
         if (!downIsFree) c++;
 
-        if (c >= 2) {  // it's a corner // todo, this is not correct?, could also be a single line
-            blobCreator.addToCornerIndexes(current);
+        if (c >= 2) {  // it's a corner // todo, could also be a single line
+            blobCreator.addCorner(current, x, y);
+            if (x < minX) { minX = x; minXCornerIndex = current;}
+            if (x > maxX) { maxX = x; maxXCornerIndex = current;}
+            if (y < minY) { minY = y; minYCornerIndex = current;}
+            if (y > maxY) { maxY = y; maxYCornerIndex = current;}
         }
 
         float valueMoveDirection, valueCurrentDirection;
 
-        while(true) {
+        while (true) {
 
-            valueMoveDirection =  thresholdChecker.check(pixels[current + moveDirection]);
+            valueMoveDirection = thresholdChecker.check(pixels[current + moveDirection]);
 
             if (valueMoveDirection >= threshold) {
 
-                blobCreator.addToEdgeIndexes(current);
+                blobCreator.addEdge(current, x, y);
 
                 current += moveDirection;
+                x = current % imageWidth;
+                y = (current - x) / imageWidth;
 
-                if (current == startIndex) {
-                    break;
-                }
+                if (current == startIndex) break;
 
-                valueCurrentDirection =  thresholdChecker.check(pixels[current + checkDirection]);
+                valueCurrentDirection = thresholdChecker.check(pixels[current + checkDirection]);
 
                 if (valueCurrentDirection >= threshold) {
 
-                    blobCreator.addToCornerIndexes(current);
+                    blobCreator.addCorner(current, x, y);
+                    if (x < minX) { minX = x; minXCornerIndex = current;}
+                    if (x > maxX) { maxX = x; maxXCornerIndex = current;}
+                    if (y < minY) { minY = y; minYCornerIndex = current;}
+                    if (y > maxY) { maxY = y; maxYCornerIndex = current;}
 
                     current += checkDirection;
+                    x = current % imageWidth;
+                    y = (current - x) / imageWidth;
 
-                    if (current == startIndex) {
-                        break;
-                    }
+                    if (current == startIndex) break;
 
                     setDirection(checkDirection);
                 }
 
 
-            }
-            else {
+            } else {
                 // we hit a wall
                 // so go right since we want to go clock wise
-                blobCreator.addToCornerIndexes(current);
+                blobCreator.addCorner(current, x, y);
+                if (x < minX) { minX = x; minXCornerIndex = current;}
+                if (x > maxX) { maxX = x; maxXCornerIndex = current;}
+                if (y < minY) { minY = y; minYCornerIndex = current;}
+                if (y > maxY) { maxY = y; maxYCornerIndex = current;}
                 turnRight();
 
             }
 
         }
 
+        blobCreator.setMinAndMaxCornerValues(minXCornerIndex, minX, minYCornerIndex, minY, maxXCornerIndex, maxX, maxYCornerIndex, maxY);
+
+        // check if it's an outerContour or not
+        // we do this by checking pixels outside the contour
+        // from the most left corner
+
+        valueDown = thresholdChecker.check(pixels[minXCornerIndex+DOWN]);
+        valueUp = thresholdChecker.check(pixels[minXCornerIndex+UP]);
+        valueLeft = thresholdChecker.check(pixels[minXCornerIndex+LEFT]);
+        // we don't have to check for right
+
+        boolean downIsFree = valueDown < threshold;
+        boolean upIsFree =  valueUp < threshold;
+        boolean leftIsFree = valueLeft < threshold;
+
+        boolean isOuterContour = upIsFree || leftIsFree || downIsFree;
+
+        blobCreator.isOuterContour(isOuterContour);
+
         blobCreator.finishContour(pixels, imageWidth, imageHeight);
+
 
     }
 
